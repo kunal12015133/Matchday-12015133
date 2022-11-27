5e15b593132275d654e738bb3639725b43a27048
KUNAL <kunal_1201533@outlook.in>
2022-11-27 23:08:46 +0530

    video Updated

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function Page2(){
-    return <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/HucIqi8Lw3E" title="YouTube video player" frameborder="0" allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
+    return <div >
+            <h2 className="vdo">Video Analytics</h2>
+            <hr className="Hr"></hr>
+            <iframe className="video"  width="560" height="560" src="https://www.youtube.com/embed/_gAMKKOyU8A" controls></iframe>
+        </div>
 }
